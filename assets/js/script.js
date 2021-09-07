5315039fa36df4646f3c2ba9be4c840961d8b18e
Bahar ismayilzade <baharsalmanova2@gmail.com>
2021-09-07 18:46:49 +0400

    newpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
-let faqItems = document.querySelectorAll(".faq .item .title");
-
-faqItems.forEach((elem) => {
-    elem.addEventListener("click", function () {
-        this.parentNode.classList.toggle("show");
-    });
-});
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })();
+  
 
 
 $('.owl-carousel').owlCarousel({
